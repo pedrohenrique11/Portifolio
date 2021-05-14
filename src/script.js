3d96516ff64854b78d36b7983f6202ae1a9de2c4
Pedro henrique <ballack.11@gmail.com>
2021-05-14 17:40:19 -0300

    add scroll animation for navbar

---
+++ src/script.js
@@ -0,0 +1,23 @@
+
+
+// menu nav scroll
+var menuItens = document.querySelectorAll ('.navbar a[href^="#"] ,.footer a[href^="#"]');
+
+menuItens.forEach(intem => {
+    intem.addEventListener('click', scrollIdClick);
+
+})
+
+function scrollIdClick(event) {
+    event.preventDefault();
+
+    const element = event.target;
+    const id = element.getAttribute('href');
+    const to = document.querySelector(id).offsetTop;
+    
+    window.scroll({
+        top: to ,
+        behavior: "smooth",
+    });
+}
+
