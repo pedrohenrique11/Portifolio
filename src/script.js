d76881b2a78ca110d6f55a098c5682377e704b10
Pedro henrique <ballack.11@gmail.com>
2021-06-08 02:30:30 -0300

    modal icons created

--- src/script.js
+++ src/script.js
@@ -19,6 +19,7 @@ function scrollIdClick(event) {
     });
 };
 
+
 // Menu mobile
 const btnMobile = document.getElementById('btn--mobile') //selecionando o botao
 
@@ -29,3 +30,35 @@ function toggleMenu() {
 
 btnMobile.addEventListener('click', toggleMenu); // evento
 
+
+// Icons modal function
+function showedModal (modalID) {
+    const modal = document.getElementById(modalID);
+    modal.classList.add('showed');
+    modal.addEventListener('click', () => modal.classList.remove('showed'))
+}
+
+//const icons
+const javaScriptModal = document.querySelector('.icon__js');
+javaScriptModal.addEventListener('click', () => showedModal('modal-js'));
+
+const htmlModal = document.querySelector('.icon__html');
+htmlModal.addEventListener('click', () => showedModal('modal-html'));
+
+const cssModal = document.querySelector('.icon__css');
+cssModal.addEventListener('click', () => showedModal('modal-css'));
+
+const reactModal = document.querySelector('.icon__react');
+reactModal.addEventListener('click', () => showedModal('modal-react'));
+
+const nodeJSModal = document.querySelector('.icon__nodejs');
+nodeJSModal.addEventListener('click', () => showedModal('modal-nodejs'));
+
+const cModal = document.querySelector('.icon__c');
+cModal.addEventListener('click', () => showedModal('modal-c'));
+
+const angularModal = document.querySelector('.icon__angular');
+angularModal.addEventListener('click', () => showedModal('modal-angular'));
+
+const JavaScriptModal2 = document.querySelector('.icon__js2');
+JavaScriptModal2.addEventListener('click', () => showedModal('modal-js'));
